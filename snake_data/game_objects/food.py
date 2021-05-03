554a4e191595aadf8c6a5512c0642b1cff4e0655
Axel Junior <alexandre_junior@id.uff.br>
2021-05-03 17:38:01 -0300

    vFix

--- snake_data/game_objects/food.py
+++ snake_data/game_objects/food.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import random
 from snake_data.toolkit import *
 from snake_data.game_objects.rectangles import *
@@ -34,4 +35,42 @@ class Food(object):
         self.__foodCollor = newCollor
 
 if __name__ == '__main__':
+=======
+import random
+from snake_data.toolkit import *
+from snake_data.game_objects.rectangles import *
+
+class Food(object):
+    def __init__(self):
+        
+        self.__rectScale = (Responsivity.rectangle_scale(),Responsivity.rectangle_scale())
+        self.__responsyHeight = Responsivity.height_screen()//Responsivity.rectangle_scale()
+        self.__responsyWidth = Responsivity.width_screen()//Responsivity.rectangle_scale()
+
+        self.__foodCollor = 'red'
+        self.__xFood = (random.randint(1, self.__responsyWidth-2))*self.__rectScale[0]
+        self.__yFood = (random.randint(1, self.__responsyHeight-2))*self.__rectScale[0]
+        self.__food = Rectangle(self.__rectScale,(self.__xFood, self.__yFood))
+        self.__food = Rectangle(self.__rectScale)
+        self.__food.update((self.__xFood, self.__yFood))
+
+    def new_food_position(self,bodyPositions,validSpaces):
+
+        validSpaces = set(validSpaces) - set(bodyPositions)
+        new_position = random.choice(list(validSpaces))
+        self.__xFood, self.__yFood = new_position
+        self.__food.update((self.__xFood, self.__yFood))
+
+    @property
+    def food(self):
+        return self.__food
+    @property
+    def foodCollor(self):
+        return self.__foodCollor
+    @foodCollor.setter
+    def foodCollor(self, newCollor):
+        self.__foodCollor = newCollor
+
+if __name__ == '__main__':
+>>>>>>> a1898a2831cad84583c5092c78c5c460e9a24d7c
     pass
--- snake_data/game_objects/snake.py
+++ snake_data/game_objects/snake.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from snake_data.game_objects.rectangles import *
 from snake_data.toolkit import *
 
@@ -82,4 +83,90 @@ class Snake(object):
         self.__orientation = newOrientation
 
 if __name__ == '__main__':
+=======
+from snake_data.game_objects.rectangles import *
+from snake_data.toolkit import *
+
+
+class Snake(object):
+    def __init__(self, startPosition):
+        self.__scale = Responsivity.rectangle_scale()
+        self.__size = 3
+        self.__snakeCollor = 'violet'
+        self.__snakeViva = True
+        self.__orientation = 'Right'
+        self.__snakeCoordinates = []
+        self.__snakeCoordinates.append(startPosition)
+        self.__xHead, self.__yHead = startPosition
+
+        self.__snakeHead = Rectangle(area=(self.__scale,self.__scale), position=startPosition)
+        self.__snakeBody = [Rectangle(area=(self.__scale,self.__scale), position=(self.__xHead-self.__scale*num,self.__yHead)) 
+                                    for num in list(range(1,self.__size))]
+
+        self.__snakeCoordinates.extend([(self.__xHead-self.__scale*num,self.__yHead) 
+                                        for num in list(range(1,self.__size))])
+
+
+    def move_snake(self):
+        self.__snakeBody.insert(0,Rectangle(area=(self.__scale,self.__scale),position=(self.__xHead,self.__yHead)))
+        self.__snakeCoordinates.insert(0,(self.__xHead,self.__yHead))
+        self.__snakeBody.pop()
+        self.__snakeCoordinates.pop()
+
+        if self.orientation == 'Left':
+            self.__xHead-=self.__scale           
+        elif self.orientation == 'Right':
+            self.__xHead+=self.__scale           
+        elif self.orientation == 'Up':
+            self.__yHead-=self.__scale           
+        elif self.orientation == 'Down':
+            self.__yHead+=self.__scale
+
+        self.__snakeHead.update((self.__xHead,self.__yHead))
+        self.__snakeCoordinates[0] = (self.__xHead,self.__yHead)            
+
+    def eating(self,foodcollor=None):
+        self.__size += 1
+        self.__snakeBody.insert(len(self.__snakeBody), Rectangle((self.__scale,self.__scale)))
+        xTail = self.__snakeBody[-2:][0].rect.x
+        yTail = self.__snakeBody[-2:][0].rect.y
+        self.__snakeBody[len(self.__snakeBody)-1].update((xTail,yTail))
+        self.__snakeCoordinates.append((xTail,yTail))
+        
+
+    @property
+    def snakeHead(self):
+        return self.__snakeHead
+    @property
+    def snakeBody(self):
+        return self.__snakeBody
+    @property
+    def snakeCoordinates(self):
+        return self.__snakeCoordinates
+    @property
+    def snakeCollor(self):
+        return self.__snakeCollor
+    @property
+    def scale(self):
+        return self.__scale
+    @property
+    def size(self):
+        return self.__size-1
+
+    @property
+    def snakeViva(self):
+        return self.__snakeViva
+    @snakeViva.setter
+    def snakeViva(self, newStatus):
+        self.__snakeViva = newStatus
+
+    @property
+    def orientation(self):
+        return self.__orientation
+    @orientation.setter
+    def orientation(self, newOrientation):
+        self.__orientation = newOrientation
+
+if __name__ == '__main__':
+>>>>>>> a1898a2831cad84583c5092c78c5c460e9a24d7c
     pass
